e02bc62c7e16da98dd763fba986e2e0919db1c43
mchach24 <hach24mc@gmail.com>
2015-08-21 15:52:55 -0400

    Added detection for letters in guess, better input for guesses. Fixed bugs.

--- guess.js
+++ guess.js
@@ -9,20 +9,26 @@ initialize();
 
 function guessNumber(guess) {
 	if (!guessCorrect) {
-		if (guess > 10 || guess < 1 ) {
-			paragraph.innerHTML = "Your guess was out of the range! Pick a number between 1 and 10.";
+		if (guess.match(/[a-z]/i)) {
+            paragraph.innerHTML = guess + " contains letters or characters that are not recognized. Pick a number between 1 and 10.";
+            return;
+        }
+        if (guess > 10 || guess < 1 ) {
+			paragraph.innerHTML = guess + " was out of the range! Pick a number between 1 and 10.";
+            return;
 		}
 		if (guess == number) {
-			paragraph.innerHTML = "You won!";
+			paragraph.innerHTML = guess + " was correct! You win!"
 			guessCorrect = true;
             initialize();
 		}
 		else if (guess < number) {
-			paragraph.innerHTML = "Your guess was too low! Try again!";
+			paragraph.innerHTML = guess + " was too low! Try again!";
 		}
 		else {
-			paragraph.innerHTML = "Your guess was too high! Try again!";
+			paragraph.innerHTML = guess + " was too high! Try again!";
 		}
+        document.getElementById('text-field').value = "";
 	}
 }
 
@@ -31,8 +37,7 @@ function testRandom() {
     for (i=0; i < 1000; i++) {
         number = Math.floor((Math.random() * 10) + 1);
         array.sort(function(a,b) { return a-b });
-        array.push(number);
-        
+        array.push(number); 
     }
     alert(array.indexOf(0) + " " + array.indexOf(11))
     paragraph.innerHTML = paragraph.innerHTML + " " + array;
